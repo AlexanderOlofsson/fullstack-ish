a838109aaccb1a90f4b6d2115f439b5437b8f1e8
AlexanderOlofsson <alex.j.olofsson@gmail.com>
2024-10-26 22:56:17 +0200

    testing

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,27 @@
+const dotenv = require('dotenv'),
+  { Client } = require('pg')
+
+  dotenv.config()
+
 const express = require('express'),
   path = require('path')
 
+  const client = new Client({
+    connectionString: process.env.PGURI
+  })
+
+  client.connect()
+
 const app = express()
 
-app.get('/api', (_request, response) => {
-  response.send({ hello: 'World' })
+app.get('/api', async (_request, response) => {
+  const { rows } = await client.query(
+    'SELECT * FROM name WHERE name',
+  )
+
+  response.send(rows)
 })
+
 
 app.use(express.static(path.join(path.resolve(), 'dist')))
 
